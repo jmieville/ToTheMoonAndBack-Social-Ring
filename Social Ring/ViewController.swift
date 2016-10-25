81753761684efbf4bf608b44b47b6d9442e77dc7
Jean-Marc Kampol Mieville <jeanmar@ebiz-admins-macbook-air-3.local>
2016-10-26 01:12:23 +0700

    Initial Commit

---
+++ Social Ring/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social Ring
+//
+//  Created by Jean-Marc Kampol Mieville on 10/26/2559 BE.
+//  Copyright © 2559 Jean-Marc Kampol Mieville. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
