fd317ac02d715ea1a5cc7cf3bb83fb4eec22e7a9
Jean-Marc Kampol Mieville <jeanmar@ebiz-admins-macbook-air-4.local>
2016-10-29 23:24:48 +0700

    Successfully added FB log in and linked it to Firebase

--- Social Ring/AppDelegate.swift
+++ Social Ring/AppDelegate.swift
@@ -7,18 +7,29 @@
 //
 
 import UIKit
+import Firebase
+import FacebookLogin
+import FacebookCore
+
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate{
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+
+        FIRApp.configure()
+        
+        
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -39,7 +50,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        AppEventsLogger.activate(application)
+
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool
+    {
+    return SDKApplicationDelegate.shared.application(app, open: url, options: options)
+    }
+    
+    
+
 
 
 }
