333d5383ede7f90e597987f3fc71c9eb8c338dc6
Jean-Marc Kampol Mieville <jeanmar@ebiz-admins-macbook-air-4.local>
2016-10-30 10:20:52 +0700

    commit

--- Social Ring/ViewController.swift
+++ Social Ring/ViewController.swift
@@ -7,19 +7,120 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
+import Firebase
+import FirebaseAuth
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var passwordField: UITextField!
+    @IBAction func signInFirebase(_ sender: AnyObject) {
+        if let email = emailField.text, let password = passwordField.text {
+            let email = emailField.text
+            let password = passwordField.text
+            firebaseAuthEmailPass(email: email!, password: password!)
+
+        } else {
+            print("Please put your email and password correctly.")
+        }
+        
+    }
+    
+
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBAction func dismissKeyboardTap(_ sender: AnyObject) {
+        dismissKeyboard()
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        emailField?.delegate = self
+        passwordField?.delegate = self
+        
+    
+        
+        
+        if let accessToken = AccessToken.current {
+            let loginButton = LoginButton(readPermissions: [ .publicProfile, .email ])
+            loginButton.center = view.center
+            view.addSubview(loginButton)
+            let credential = FIRFacebookAuthProvider.credential(withAccessToken: (AccessToken.current?.authenticationToken)!)
+            print("Getting the \(credential)")
+            self.firebaseAuth(credential)
+
+        } else {
+            let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+            loginButton.center = view.center
+            view.addSubview(loginButton)
+            print("proceed to log-in")
+        }
+        
+//        func textFieldShouldReturn(textField: UITextField) -> Bool {
+//            self.emailField.resignFirstResponder()
+//            self.passwordField.resignFirstResponder()
+//            return true
+//        }
+        
     }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential) { (user, error) in
+            if error != nil {
+                print("\(user?.email) unsuccessfully log in with firebase - \(error)")
+            } else {
+                print("\(user?.email) successfully logged in with firebase - \(error)")
+            }
+        }
+        
+    }
+    
+    func firebaseAuthEmailPass(email: String, password: String){
+        FIRAuth.auth()?.signIn(withEmail: emailField.text!, password: passwordField.text!, completion: { (user, error) in
+            if error != nil {
+                print("\(user?.email) unsuccessfully log in with firebase - \(error)")
+                FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                    if error != nil {
+                        print("Failed to create user")
+                    } else {
+                        print("Successfully created user")
+                        
+                    }
+                })
+            } else {
+                print("\(user?.email) successfully logged in with firebase - \(error)")
+
+            }
+        })
+    }
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with:event)
+        self.view.endEditing(true)
+    }
+    
 
 }
 
